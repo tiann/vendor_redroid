cd72bea9ab38f0055d94e5cb50f2bb517913be6b
Ziyang Zhou <ziyang.zhou@outlook.com>
2020-12-20 16:45:29 +0800

    ReDroid 11 codec

--- redroid.11.mk
+++ redroid.11.mk
@@ -18,64 +18,72 @@ AUDIOSERVER_MULTILIB := first
 
 # ?
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.radio.noril=yes
+    ro.radio.noril=yes
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.sf.nobootanimation=1
+    debug.sf.nobootanimation=1
 
 # required by sync.sh script
 PRODUCT_HOST_PACKAGES += \
-	fs_config
+    fs_config
 
 PRODUCT_PACKAGES += \
-	vncserver \
-	hwcomposer.redroid \
-	gralloc.redroid \
-	libEGL_swiftshader \
-	libGLESv1_CM_swiftshader \
-	libGLESv2_swiftshader \
+    vncserver \
+    hwcomposer.redroid \
+    gralloc.redroid \
+    libEGL_swiftshader \
+    libGLESv1_CM_swiftshader \
+    libGLESv2_swiftshader \
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.kernel.qemu=1 \
-	ro.hardware=redroid \
-	ro.sf.lcd_density=320 \
-	persist.sys.fuse=1 \
+    ro.kernel.qemu=1 \
+    ro.hardware=redroid \
+    ro.sf.lcd_density=320 \
+    persist.sys.fuse=1 \
 
 # Phone App required
 PRODUCT_PACKAGES += \
-	rild
+    rild
 
 # WiFi required by SystemUI
 PRODUCT_PACKAGES += \
-	android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
 
 
 # required HIDL
 PRODUCT_PACKAGES += \
-	vndservicemanager \
-	android.hardware.audio.service \
-	android.hardware.audio@4.0-impl \
-	android.hardware.audio.effect@4.0-impl \
-	android.hardware.drm@1.0-service \
-	android.hardware.drm@1.0-impl \
-	android.hardware.gatekeeper@1.0-service.software \
-	android.hardware.graphics.allocator@2.0-service \
-	android.hardware.graphics.allocator@2.0-impl \
-	android.hardware.graphics.mapper@2.0-impl-2.1 \
-	android.hardware.graphics.composer@2.1-service \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.health@2.1-service \
-	android.hardware.health@2.1-impl \
-	android.hardware.keymaster@3.0-service \
-	android.hardware.keymaster@3.0-impl \
+    vndservicemanager \
+    android.hardware.audio.service \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-impl \
+    android.hardware.gatekeeper@1.0-service.software \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
 
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/boot_completed.redroid.sh:system/bin/boot_completed.redroid.sh \
-	frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/boot_completed.redroid.sh:system/bin/boot_completed.redroid.sh \
+
+    
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/camera/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    device/generic/goldfish/camera/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
 
