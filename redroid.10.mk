460d398e5ff1ffe6c72078766f41f7c4b7f72e6a
Ziyang Zhou <ziyang.zhou@outlook.com>
2020-12-20 16:43:45 +0800

    ReDroid 10 codec

--- redroid.10.mk
+++ redroid.10.mk
@@ -16,70 +16,79 @@ AUDIOSERVER_MULTILIB := first
 
 # ?
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.radio.noril=yes
+    ro.radio.noril=yes
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.sf.nobootanimation=1
+    debug.sf.nobootanimation=1
 
 # required by sync.sh script
 PRODUCT_HOST_PACKAGES += \
-	fs_config
+    fs_config
 
 PRODUCT_PACKAGES += \
-	vncserver \
-	hwcomposer.redroid \
-	gralloc.redroid \
-	gatekeeper.ranchu \
-	libEGL_swiftshader \
-	libGLESv1_CM_swiftshader \
-	libGLESv2_swiftshader \
+    vncserver \
+    hwcomposer.redroid \
+    gralloc.redroid \
+    gatekeeper.ranchu \
+    libEGL_swiftshader \
+    libGLESv1_CM_swiftshader \
+    libGLESv2_swiftshader \
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.kernel.qemu=1 \
-	ro.hardware=redroid \
-	ro.hardware.gatekeeper=ranchu \
-	ro.sf.lcd_density=320 \
+    ro.kernel.qemu=1 \
+    ro.hardware=redroid \
+    ro.hardware.gatekeeper=ranchu \
+    ro.sf.lcd_density=320 \
 
 # Phone App required
 PRODUCT_PACKAGES += \
-	rild
+    rild
 
 # WiFi required by SystemUI
 PRODUCT_PACKAGES += \
-	android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
 
 
 # required HIDL
 PRODUCT_PACKAGES += \
-	android.hardware.audio@2.0-service \
-	android.hardware.audio@4.0-impl \
-	android.hardware.audio.effect@4.0-impl \
-	android.hardware.drm@1.0-service \
-	android.hardware.drm@1.0-impl \
-	android.hardware.gatekeeper@1.0-service \
-	android.hardware.gatekeeper@1.0-impl \
-	android.hardware.graphics.allocator@2.0-service \
-	android.hardware.graphics.allocator@2.0-impl \
-	android.hardware.graphics.mapper@2.0-impl-2.1 \
-	android.hardware.graphics.composer@2.1-service \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.health@2.0-service.goldfish \
-	android.hardware.keymaster@3.0-service \
-	android.hardware.keymaster@3.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.health@2.0-service.goldfish \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
 
 USE_XML_AUDIO_POLICY_CONF := 1
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/boot_completed.redroid.sh:system/bin/boot_completed.redroid.sh \
+    $(LOCAL_PATH)/boot_completed.redroid.sh:system/bin/boot_completed.redroid.sh \
+
+
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/camera/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_configuration_generic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/surround_sound_configuration_5_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/surround_sound_configuration_5_0.xml \
+
 
 # Extend heap size we use for dalvik/art runtime
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
