af6da6aec87572e7b42c9d2e79221226a7dc9f79
Ziyang Zhou <ziyang.zhou@outlook.com>
2020-12-20 17:02:55 +0800

    ReDroid 8 codec profiles

--- redroid.8.mk
+++ redroid.8.mk
@@ -16,72 +16,75 @@ AUDIOSERVER_MULTILIB := first
 
 # ?
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.radio.noril=yes
+    ro.radio.noril=yes
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.sf.nobootanimation=1
+    debug.sf.nobootanimation=1
 
 # required by sync.sh script
 PRODUCT_PACKAGES += \
-	fs_config
+    fs_config
 
 PRODUCT_PACKAGES += \
-	vncserver \
-	hwcomposer.redroid \
-	gralloc.redroid \
-	gatekeeper.ranchu \
-	libEGL_swiftshader \
-	libGLESv1_CM_swiftshader \
-	libGLESv2_swiftshader \
+    vncserver \
+    hwcomposer.redroid \
+    gralloc.redroid \
+    gatekeeper.ranchu \
+    libEGL_swiftshader \
+    libGLESv1_CM_swiftshader \
+    libGLESv2_swiftshader \
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.kernel.qemu=1 \
-	ro.hardware=redroid \
-	ro.hardware.gatekeeper=ranchu \
-	ro.sf.lcd_density=320 \
+    ro.kernel.qemu=1 \
+    ro.hardware=redroid \
+    ro.hardware.gatekeeper=ranchu \
+    ro.sf.lcd_density=320 \
 
 # Phone App required
 PRODUCT_PACKAGES += \
-	rild
+    rild
 
 # WiFi required by SystemUI
 PRODUCT_PACKAGES += \
-	android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
 
 
 # required HIDL
 PRODUCT_PACKAGES += \
-	android.hardware.audio@2.0-service \
-	android.hardware.audio@2.0-impl \
-	android.hardware.audio.effect@2.0-impl \
-	android.hardware.configstore@1.1-service \
-	android.hardware.drm@1.0-service \
-	android.hardware.drm@1.0-impl \
-	android.hardware.gatekeeper@1.0-service \
-	android.hardware.gatekeeper@1.0-impl \
-	android.hardware.graphics.allocator@2.0-service \
-	android.hardware.graphics.allocator@2.0-impl \
-	android.hardware.graphics.composer@2.1-service \
-	android.hardware.graphics.composer@2.1-impl \
-	android.hardware.graphics.mapper@2.0-impl \
-	android.hardware.keymaster@3.0-service \
-	android.hardware.keymaster@3.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.configstore@1.1-service \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
 
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/boot_completed.redroid.sh:system/bin/boot_completed.redroid.sh \
+    $(LOCAL_PATH)/boot_completed.redroid.sh:system/bin/boot_completed.redroid.sh \
 
 
 PRODUCT_COPY_FILES += \
-	device/generic/goldfish/camera/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
+    device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
+    device/generic/goldfish/camera/media_codecs.xml:system/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf \
+    device/generic/goldfish/audio_policy.conf:system/etc/audio_policy.conf
 
-	
+    
 # Extend heap size we use for dalvik/art runtime
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 
