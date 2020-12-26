1664eb1f70ca811a0328d6eb1e5b97285fc7ed5c
Ziyang Zhou <ziyang.zhou@outlook.com>
2020-12-27 01:19:31 +0800

    fix for ReDroid 10

--- redroid.10.mk
+++ redroid.10.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += external/mesa3d
+
 AUDIOSERVER_MULTILIB := first
 
 # ?
